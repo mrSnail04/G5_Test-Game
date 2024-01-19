f952160f27f67be9ec54dd34674917d964684bb7
mrSnail04 <maks.bulavsky@gmail.com>
2024-01-19 18:25:09 +0300

    gh-pages

--- webpack/prod.js
+++ webpack/prod.js
@@ -5,8 +5,8 @@ const TerserPlugin = require("terser-webpack-plugin");
 module.exports = merge.merge(base, {
   mode: "production",
   output: {
-    filename: "bundle.js",
-    path: '/',
+    filename: "bundle.min.js",
+    publicPath: '/',
   },
   devtool: false,
   performance: {
